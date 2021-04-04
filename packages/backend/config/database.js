b206202538751d6b02569ca06df020c6ccae2a7e
Grigory Vlasov <grisha.vlasov92@gmail.com>
2021-04-04 19:47:47 +0700

    add docker-compose and create article and comment collection types

--- packages/backend/config/database.js
+++ packages/backend/config/database.js
@@ -1,18 +1,20 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'blog-dev'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'Uhbif771!'),
-        ssl: env.bool('DATABASE_SSL', false),
+module.exports = ({ env }) => {
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host: env("DATABASE_HOST"),
+          port: env.int("DATABASE_PORT"),
+          database: env("DATABASE_NAME"),
+          username: env("DATABASE_USERNAME"),
+          password: env("DATABASE_PASSWORD"),
+          ssl: env.bool("DATABASE_SSL"),
+        },
+        options: {},
       },
-      options: {}
     },
-  },
-});
+  };
+};
